2bc9a81847e2714db6e64b63150b9a38935bb69b
Kelvin Li <seawaywen@gmail.com>
2018-07-07 17:30:41 +0800

    update the dockerFile and related config files.

--- .docker/app/docker-entrypoint.sh
+++ .docker/app/docker-entrypoint.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-DJANGO_SETTINGS_MODULE=django_project.settings_build python3 django_project/manage.py collectstatic --noinput \
-    > logs/collectstatic.log 2>&1 || (cat logs/collectstatic.log && false)
-
+#DJANGO_SETTINGS_MODULE=django_project.settings_build python3 django_project/manage.py collectstatic --noinput \
+#    > logs/collectstatic.log 2>&1 || (cat logs/collectstatic.log && false)
 
 python3 django_project/manage.py migrate
-DEVEL=1 talisker --reload --bind=0.0.0.0:9000 --workers=2 django_project.wsgi:application
 
-# development mode
-#python django_project/manage.py runserver 0.0.0.0:9000
+DEVEL=1 talisker --reload --bind=0.0.0.0:9000 --workers=2 django_project.wsgi:application
