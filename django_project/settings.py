97be7fb7b6e601b965435ca5b1ab8aec624907bf
Kelvin Li <seawaywen@gmail.com>
2018-07-15 23:14:04 +0800

    integrate the gunMail service to send the mail via the their provided APIs; and update the kubernetes settings as well.

--- django_project/settings.py
+++ django_project/settings.py
@@ -6,4 +6,15 @@ from django_project.settings_devel import *
 
 TEMPLATE_DEBUG = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+    EMAIL_HOST = os.environ.get('EMAIL_HOST', '')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', '')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+    EMAIL_PORT = 587
+    EMAIL_USE_TLS = True
+
+
